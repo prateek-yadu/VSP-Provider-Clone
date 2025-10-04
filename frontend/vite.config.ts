386f9a849b3f1a5feab48f43983ad64c221553c4
Prateek Yadu <prateekyadudotdev@gmail.com>
2025-10-04 17:59:36 +0530

    feat(frontend): added shadcn support

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from "path" // needed for shadcn to work properly
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),
-],
+  ],
+
+  /* shadcn support */
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+
 })
