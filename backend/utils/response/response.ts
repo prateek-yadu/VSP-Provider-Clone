63fa792b11fe0f2c86d50de414fa59a6109a08a7
Prateek Yadu <prateekyadudotdev@gmail.com>
2025-10-18 17:29:48 +0530

    chore(backend): added status in modular response

--- backend/utils/response/response.ts
+++ backend/utils/response/response.ts
@@ -5,12 +5,14 @@ const send = {
     */
     ok(res: Response, message?: string, data?: any) {
         return res.status(200).json({
+            status: 200,
             message: message ? message : "OK",
             ...(data && { data })
         })
     },
     created(res: Response, message?: string, data?: any) {
         return res.status(201).json({
+            status: 201,
             message: message ? message : "Created",
             ...(data && { data })
         })
@@ -21,24 +23,28 @@ const send = {
    */
     badRequest(res: Response, message?: string, data?: any) {
         return res.status(400).json({
+            status: 400,
             message: message ? message : "Bad Request",
             ...(data && { data })
         })
     },
     unauthorized(res: Response, message?: string, data?: any) {
         return res.status(401).json({
+            status: 401,
             message: message ? message : "Unauthorized",
             ...(data && { data })
         })
     },
     forbidden(res: Response, message?: string, data?: any) {
         return res.status(403).json({
+            status: 403,
             message: message ? message : "Forbidden",
             ...(data && { data })
         })
     },
     notFound(res: Response, message?: string, data?: any) {
         return res.status(404).json({
+            status: 404,
             message: message ? message : "Not Found",
             ...(data && { data })
         })
@@ -49,6 +55,7 @@ const send = {
    */
     internalError(res: Response, message?: string, data?: any) {
         return res.status(500).json({
+            status: 500,
             message: message ? message : "Internal Server Error",
             ...(data && { data })
         })
