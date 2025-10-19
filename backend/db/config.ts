ace755e11f9561451190f3f6faa42aa4c6fdbe22
Prateek Yadu <prateekyadudotdev@gmail.com>
2025-10-19 14:12:31 +0530

    fix(backend): made db to work with environment variables

--- backend/db/config.ts
+++ backend/db/config.ts
@@ -1,10 +1,10 @@
-const mysql = require('mysql2');
+import mysql from 'mysql2/promise';
+import 'dotenv/config'
 
-// Create the connection pool. The pool-specific settings are the defaults
 export const pool = mysql.createPool({
-    host: `${process.env.DB_HOST}`,
-    user: `${process.env.DB_USER}`,
-    database: `${process.env.DB_NAME}`,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
     maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
@@ -12,7 +12,6 @@ export const pool = mysql.createPool({
     queueLimit: 0,
     enableKeepAlive: true,
     keepAliveInitialDelay: 0,
-    port: `${process.env.DB_PORT}`,
-    // port: process.env.DB_PORT || 3306,
-    password: `${process.env.DB_PASSWD}`
+    port: typeof(process.env.DB_PORT) == "string" && parseInt(process.env.DB_PORT) || 3306,
+    password: process.env.DB_PASSWD
 });
