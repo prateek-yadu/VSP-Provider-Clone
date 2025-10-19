524c183044805fb91282c15c3759dc0350681757
Prateek Yadu <prateekyadudotdev@gmail.com>
2025-10-19 18:02:25 +0530

    feat(backend): created login route

--- backend/controller/auth.controller.ts
+++ backend/controller/auth.controller.ts
@@ -3,15 +3,42 @@ import { v4 as uuidv4 } from 'uuid';
 import { Request, Response } from "express";
 import send from "../utils/response/index.js";
 import { pool } from '../db/config.js';
+import jwt from 'jsonwebtoken'
 
 export const Login = async (req: Request, res: Response) => {
+    const { email, password } = req.body;
     try {
-        // check the password
-        // genrate tokem if password matches
+        // checks if user exists
+        const [exists, fields]: any = await pool.query('SELECT * FROM users WHERE email = ?', [email]);
+        if (exists.length != 0) {
+            // checks password is vailed or not
+            const vailed = await bcrypt.compare(password, exists[0].password);
+
+            // if password is vailed send genrate and send token else thow error
+            if (vailed) {
+                // genrate token
+                const token = jwt.sign({ id: exists[0].id, name: exists[0].name }, process.env.JWT_SECRET || "shhh...", {
+                    expiresIn: '1d'
+                });
+
+                // sets cookie 
+                res.cookie("token", token, {
+                    httpOnly: true,
+                    maxAge: 24 * 60 * 60 * 1000, // vailed till 24 hrs
+                    secure: process.env.IN_PROD == "true" && true || false
+                });
+
+                // sends response
+                send.ok(res, "User Authenticated Successfully");
+            } else {
+                send.unauthorized(res, "Invailed Credentials");
+            }
+        } else {
+            send.unauthorized(res, "Invailed Credentials");
+        }
     } catch (error) {
         send.internalError(res);
     }
-    res.send("Login Route");
 }
 
 export const Register = async (req: Request, res: Response) => {
