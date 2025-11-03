28eccb1640113eb93b6a735823335b9e6ccb87aa
Prateek Yadu <prateekyadudotdev@gmail.com>
2025-11-03 20:40:45 +0530

    auth routes can be accessed via post request

--- backend/routes/auth/auth.routes.ts
+++ backend/routes/auth/auth.routes.ts
@@ -3,9 +3,9 @@ import { Login, Register } from "../../controller/auth.controller.js";
 
 const router = Router();
 
-router.get('/login', Login)
+router.post('/login', Login)
 
-router.get('/register', Register)
+router.post('/register', Register)
 
 
 export default router;
