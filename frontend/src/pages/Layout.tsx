faa9c99ff1a4fc30e7d074d4a9764f935a016fa6
Prateek Yadu <prateekyadudotdev@gmail.com>
2025-11-04 14:11:18 +0530

    feat(frontend): added redux toolkit & auth context

--- frontend/src/pages/Layout.tsx
+++ frontend/src/pages/Layout.tsx
@@ -1,14 +1,18 @@
 import { Outlet } from 'react-router'
 import { ThemeProvider } from '../components/custom/theme-provider'
 import { Toaster } from '../components/ui/sonner'
+import { store } from '../app/store'
+import { Provider } from 'react-redux'
 
 export default function Layout() {
     return (
         <>
-            <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-                <Outlet />
-                <Toaster position='top-center' richColors/>
-            </ThemeProvider>
+            <Provider store={store}>
+                <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+                    <Outlet />
+                    <Toaster position='top-center' richColors />
+                </ThemeProvider>
+            </Provider>
         </>
     )
 }
